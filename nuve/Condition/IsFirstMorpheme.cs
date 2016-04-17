423b2480783f10057c309261dd8c83b48c434e80
hrzafer <harunzafer@gmail.com>
2016-04-17 23:14:41 +0300

    Add new Condition IsFirstMorpheme

---
+++ nuve/Condition/IsFirstMorpheme.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nuve.Morphologic.Structure;
+using Nuve.Orthographic;
+
+namespace Nuve.Condition
+{
+    internal class IsFirstMorpheme : ConditionBase
+    {
+        public IsFirstMorpheme(string position, string operand, Alphabet alphabet) : base(position, operand, alphabet)
+        {
+        }
+
+        public override bool IsTrueFor(Allomorph allomorph)
+        {
+            Allomorph operand;
+            if (TryGetOperandMorpheme(allomorph, out operand))
+            {
+                return !operand.HasPrevious;
+            }
+
+            return false;
+        }
+    }
+}
